858d1899f3c31b01d5012560012058b9e1a7d6fd
Adii <itsaditya101@gmail.com>
2026-05-29 15:23:14 +0530

    check again

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ function App() {
 
   return (
     <>
-      <h1>Github Actions</h1>
-      <h2>Complete course with 2 projects</h2>
+      <h1>Learning Github Actions</h1>
+      <h2>Adiii</h2>
     </>
   )
 }
