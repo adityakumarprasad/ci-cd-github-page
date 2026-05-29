7f58690b2ab2653d48b07f2f864dae8253adcd3c
Adii <itsaditya101@gmail.com>
2026-05-29 16:58:43 +0530

    final check

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/ci-cd-github-page/',
   test: {
     environment: 'jsdom',
     setupFiles: ['./vitest.setup.js'],
-  },
-  base: '/ci-cd-github-page/',
-  test: {
     coverage: {
       provider: 'v8',
       reporter: ['lcov', 'text'],   // lcov is what SonarCloud reads
